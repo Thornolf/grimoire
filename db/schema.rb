87a361a62535224a09adf6a886fb6e2cda693579
Quentin 'Thornolf' Baudet <thornolf@hey.com>
2024-09-13 00:20:52 +0300

    feature (missions): create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_12_205353) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_12_211945) do
+  create_table "missions", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "game_master_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_master_id"], name: "index_missions_on_game_master_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +33,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_12_205353) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "missions", "game_masters"
 end
