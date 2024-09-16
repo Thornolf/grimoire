faf2972781daca71b1c737b353d5be15e83441fc
Quentin 'Thornolf' Baudet <thornolf@hey.com>
2024-09-16 14:08:44 +0300

    fix (handouts): modal opens with content

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,12 +4,10 @@ export default class extends Controller {
   static targets = ["modal"]
 
   toggle() {
-    console.log("toggle")
     this.modalTarget.classList.toggle("hidden");
   }
 
   hide() {
-    console.log("hide")
     this.modalTarget.classList.add("hidden");
   }
 }
