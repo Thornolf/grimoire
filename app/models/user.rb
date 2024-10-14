01e6e7d1e26dd9382168616956dd39c588c3dc86
Quentin 'Thornolf' Baudet <thornolf@hey.com>
2024-10-14 11:51:10 +0300

    feature (users): avatar img defautl for gm & player

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,18 @@ class User < ApplicationRecord
 
   has_many :missions_users
   has_many :missions, through: :missions_users
-
   has_many :character_sheets
-
   has_many :notes, dependent: :destroy
+
+  has_one_attached :profile_picture
+
+  def display_profile_picture
+    if profile_picture.attached?
+      profile_picture
+    elsif game_master?
+      ActionController::Base.helpers.asset_path("game_master_default.png")
+    else
+      ActionController::Base.helpers.asset_path("player_default.png")
+    end
+  end
 end
